208f8feb2787bf7133cd5f95f74d500ebdcad9bc
RyoNakano <naca.11868@gmail.com>
2021-01-03 01:43:08 +0900

    お菓子検索Json取得まで

--- Sweets/ViewController.swift
+++ Sweets/ViewController.swift
@@ -7,13 +7,62 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UISearchBarDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        searchText.delegate = self
+        searchText.placeholder = "お菓子の名前を入力してください"
+    }
+    
+    struct ItemJson: Codable {
+        let name: String?
+        let url: URL?
+        let image: URL?
+    }
+    
+    struct ResultJson: Codable {
+        let item: [ItemJson]?
     }
 
-
+    @IBOutlet weak var searchText: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        view.endEditing(true)
+        
+        if let searchWord = searchBar.text {
+            print(searchWord)
+            searchOkashi(keyword: searchWord)
+        }
+    }
+    
+    func searchOkashi(keyword: String) {
+        guard let keyword_encode = keyword.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else {
+            return
+        }
+        guard let req_url = URL(string:
+                "https://www.sysbird.jp/toriko/api?apikey=guest&format=json&keyword=\(keyword_encode)&max=10&order=r") else {
+            return
+        }
+        print(req_url)
+        
+        let req = URLRequest(url: req_url)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: req, completionHandler: {
+            (data, responce, error) in
+            session.finishTasksAndInvalidate()
+            do {
+                let decoder = JSONDecoder()
+                let json = try decoder.decode(ResultJson.self, from: data!)
+                //print(json)
+            } catch {
+                print("error occured")
+            }
+        })
+        task.resume()
+    }
+    
 }
 
